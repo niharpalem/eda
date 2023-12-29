8b57041826e78fb7b8646924bdb443e5a8119962
niharpalem <74230650+niharpalem@users.noreply.github.com>
2023-12-29 15:34:51 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 def main():
-    st.title('Smart EDA App')
-    st.markdown("Upload a CSV file and this app will perform basic exploratory data analysis (EDA) on it.")
+    st.markdown("""
+    <style>
+    body {
+        background-image: url("https://blog.carlow.edu/wp-content/uploads/sites/26/2022/04/how-artificial-intelligence-is-shaping-data-analytics.jpg");
+        background-size: cover;
+    }
+    </style>
+    """, unsafe_allow_html=True)
+
+    st.title('Advanced EDA App')
+    st.markdown("Upload a CSV file and this app will perform advanced exploratory data analysis (EDA) on it.")
 
     uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
 
@@ -24,6 +35,9 @@ def main():
         if st.checkbox('Show summary'):
             st.table(data.describe())
 
+        if st.checkbox('Show data types'):
+            st.table(data.dtypes)
+
         st.markdown("### Variable Plots")
         if st.checkbox('Show plots'):
             st.markdown("#### Categorical Variables")
@@ -41,6 +55,30 @@ def main():
             var1, var2 = st.selectbox('Choose the first variable', num_vars), st.selectbox('Choose the second variable', num_vars)
             fig = px.scatter(data, x=var1, y=var2)
             st.plotly_chart(fig)
+
+        st.markdown("### Advanced Analysis")
+        if st.checkbox('Show correlation heatmap'):
+            corr = data.corr()
+            fig, ax = plt.subplots(figsize=(5,5))
+            sns.heatmap(corr, annot=True, ax=ax)
+            st.pyplot(fig)
+
+        if st.checkbox('Show pairplot'):
+            fig = sns.pairplot(data)
+            st.pyplot(fig)
+
+        st.markdown("### Custom Plot")
+        if st.checkbox('Create a custom plot'):
+            plot_types = ['scatter', 'line', 'area', 'bar', 'box', 'histogram']
+            plot_type = st.selectbox('Select plot type', plot_types)
+            x_var = st.selectbox('Select x variable', data.columns)
+            y_var = st.selectbox('Select y variable', data.columns)
+            fig = px.__dict__[plot_type](data, x=x_var, y=y_var)
+            st.plotly_chart(fig)
+
+    st.markdown("---")
+    st.markdown("#### Connect with me:")
+    st.markdown("[GitHub](https://github.com/niharpalem) | [Medium](https://medium.com/@nihar-palem)")
 
 if __name__ == "__main__":
     main()
