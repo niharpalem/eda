30da720211998d6143b699b5e14665ce3e033b6b
niharpalem <74230650+niharpalem@users.noreply.github.com>
2023-12-29 15:24:31 -0800

    Update app.py

--- app.py
+++ app.py
@@ -3,38 +3,44 @@ import pandas as pd
 import plotly.express as px
 
 def main():
-    st.title('CSV File Reader')
+    st.title('Smart EDA App')
+    st.markdown("Upload a CSV file and this app will perform basic exploratory data analysis (EDA) on it.")
 
     uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
 
     if uploaded_file is not None:
         data = pd.read_csv(uploaded_file, engine='python')
+        st.markdown("### Data Overview")
+        if st.checkbox('Show sample data'):
+            number = st.slider('Number of rows to view', min_value=1, max_value=50)
+            st.dataframe(data.head(number))
 
-        if st.button('Show first 5 rows'):
-            st.write(data.head())
+        if st.checkbox('Show columns'):
+            st.write(data.columns)
 
-        if st.button('Show columns and data types'):
-            st.write(data.dtypes)
+        if st.checkbox('Show shape'):
+            st.write(data.shape)
 
-        if st.button('Describe the data'):
-            st.write(data.describe())
+        if st.checkbox('Show summary'):
+            st.table(data.describe())
 
-        chart_type = st.selectbox('Select a chart type', ['Line', 'Bar', 'Area'])
-        x_axis = st.selectbox('Select x-axis', data.columns)
-        y_axis = st.selectbox('Select y-axis', data.columns)
-        color = st.color_picker('Pick A Color', '#00f900')
+        st.markdown("### Variable Plots")
+        if st.checkbox('Show plots'):
+            st.markdown("#### Categorical Variables")
+            select_box = st.selectbox('Choose one of the categorical variables', data.select_dtypes(include=['object']).columns)
+            fig = px.histogram(data, x=select_box)
+            st.plotly_chart(fig)
 
-        if st.button('Create a plot'):
-            try:
-                if chart_type == 'Line':
-                    fig = px.line(data, x=x_axis, y=y_axis, color_discrete_sequence=[color])
-                elif chart_type == 'Bar':
-                    fig = px.bar(data, x=x_axis, y=y_axis, color_discrete_sequence=[color])
-                elif chart_type == 'Area':
-                    fig = px.area(data, x=x_axis, y=y_axis, color_discrete_sequence=[color])
-                st.plotly_chart(fig)
-            except Exception as e:
-                st.write(f"An error occurred: {e}")
+            st.markdown("#### Numerical Variables")
+            select_box = st.selectbox('Choose one of the numerical variables', data.select_dtypes(include=['int64', 'float64']).columns)
+            fig = px.histogram(data, x=select_box)
+            st.plotly_chart(fig)
+
+            st.markdown("#### Scatter Plot")
+            num_vars = data.select_dtypes(include=['int64', 'float64']).columns
+            var1, var2 = st.selectbox('Choose the first variable', num_vars), st.selectbox('Choose the second variable', num_vars)
+            fig = px.scatter(data, x=var1, y=var2)
+            st.plotly_chart(fig)
 
 if __name__ == "__main__":
     main()
